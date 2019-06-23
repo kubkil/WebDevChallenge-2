ca5e2a77e70d09554f50eb69a06f59bf365ae01a
Jakub Kilar <kuba.kilar@gmail.com>
2019-06-23 16:28:58 +0200

    Change toggleClass to addClass

--- src/script.js
+++ src/script.js
@@ -3,5 +3,5 @@ var btnGallery = document.querySelector('.gallery__button');
 var galleryHidden = document.querySelector('.gallery__row--hidden');
 
 $(btnGallery).click(() => {
-  $(galleryHidden).toggleClass('gallery__row--shown');
+  $(galleryHidden).addClass('gallery__row--shown');
 });

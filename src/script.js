2b04a46f9e4be218713bc6615c1817100a805073
Jakub Kilar <kuba.kilar@gmail.com>
2019-07-30 21:09:46 +0200

    Toggle blog posts

--- src/script.js
+++ src/script.js
@@ -5,4 +5,11 @@ var galleryHidden = document.querySelector('.gallery__hidden');
 $(btnGallery).click(() => {
   $(galleryHidden).addClass('gallery__shown');
 });
+
+// toggle blog posts
+var btnBlog = document.querySelector('.blog__button');
+var postHidden = document.querySelectorAll('.blog__hidden');
+
+$(btnBlog).click(() => {
+  $(postHidden).addClass('blog__shown');
 });
